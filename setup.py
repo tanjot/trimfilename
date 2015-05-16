f1d1eb79eaeccb5f8c1d6b949b8928e9e7fac997
Tanjot Kaur <tanjot28@gmail.com>
2015-05-16 23:35:00 +0530

    Change description string

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     name="trimfilename",
     version="0.5.1",
     license='GPLv3',
-    description="Remove string patterns from filenames",
+    description="Remove string pattern from filenames",
     url="https://github.com/tanjot/trimfilename",
     author="Tanjot Kaur",
     author_email="tanjot28@gmail.com",
