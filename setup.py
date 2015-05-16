1a8e5456bda4443b29a72cf57e5c20e3f563c526
Tanjot Kaur <tanjot28@gmail.com>
2015-05-17 00:33:56 +0530

    Assign long_description to setup description

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     name="trimfilename",
     version="0.5.1",
     license='GPLv3',
-    description="Remove string pattern from filenames",
+    description=long_description,
     url="https://github.com/tanjot/trimfilename",
     author="Tanjot Kaur",
     author_email="tanjot28@gmail.com",
