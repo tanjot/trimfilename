fd1a28f5c84d56b4d68695e9e5cfc23e8f0e3108
Tanjot Kaur <tanjot28@gmail.com>
2015-05-16 18:38:17 +0530

    Add files and directories for pypi submission

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+
+    name="trimfilename",
+    version="0.5",
+    license='GPLv3',
+    description="Remove string patterns from filenames",
+    url="https://github.com/tanjot/trimfilename",
+    author="Tanjot Kaur",
+    author_email="tanjot28@gmail.com",
+    packages=['trimfilename']
+)
