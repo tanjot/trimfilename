666b0ac0120c65a0d336878bbb08a7a32ad03ed4
Tanjot Kaur <tanjot28@gmail.com>
2015-05-19 00:11:44 +0530

    Add description in setup.py

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ except (IOError, ImportError):
     fhan = open('README.txt')
     long_description = fhan.read()
     fhan.close()
-
 setup(
     name="trimfilename",
     version="0.6.0",
     license='GPLv3',
+    description="Trim useless characters from filenames",
     long_description=long_description,
     url="https://github.com/tanjot/trimfilename",
     author="Tanjot Kaur",
