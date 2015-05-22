1a2a08120970c61754d0c35c3cd12366263e320e
Tanjot Kaur <tanjot28@gmail.com>
2015-05-23 00:03:51 +0530

    Script to create test directory
    In order to remove the hassle of creating a test directory, this script will
    create a test directory with all the files specified in list created inside the
    script itself.

---
+++ create_test_firectory.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import os
+
+def main():
+    test_folder = 'test'
+    if os.path.isdir(test_folder) is False:
+        os.mkdir(test_folder)
+
+    files_to_create = ['123abc.abc', 'blaabc.blamp3', 'abc.abc']
+
+    for file in files_to_create:
+        os.system('touch ' + os.path.join(test_folder, file))
+
+
+if __name__ == '__main__':
+    main()
