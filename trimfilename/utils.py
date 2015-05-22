242166c8a7010b1d32777ea9c563a85b26d5897a
Tanjot Kaur <tanjot28@gmail.com>
2015-05-22 22:21:44 +0530

    Stop hampering file extension with -p argument
    Earlier, a match for pattern to be removed in file's extension used to change
    the extension and hence, corrupt the file. Now, the file is not renamed if there is a
    change reflected in file's extension after pattern removal

--- trimfilename/utils.py
+++ trimfilename/utils.py
@@ -11,15 +11,17 @@ def rename_file(oldname, newname, dirPath):
     '''
 
     changed_file_name = False
-    old_file_path = os.path.join(dirPath, oldname)
 
-    #Does not rename file if it begins with '.' or the whole file name gets
-    #deleted after rename and also if there is no change in filename
-    if newname and newname[0] != '.' and oldname != newname:
-        os.rename(old_file_path, os.path.join(dirPath, newname))
-        #print('Successfully renamed '+pathNname+' to'
-        #       ' '+ newname)
+    if oldname.rfind('.') == newname.rfind('.'):
+        old_file_path = os.path.join(dirPath, oldname)
 
-        changed_file_name = True
+        #Does not rename file if it begins with '.' or the whole file name gets
+        #deleted after rename and also if there is no change in filename
+        if newname and newname[0] != '.' and oldname != newname:
+            os.rename(old_file_path, os.path.join(dirPath, newname))
+            #print('Successfully renamed '+pathNname+' to'
+            #       ' '+ newname)
+
+            changed_file_name = True
 
     return changed_file_name
