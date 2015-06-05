6f261303b0fca549197118f01213998c4cefff0e
Tanjot Kaur <tanjot28@gmail.com>
2015-06-05 08:31:30 +0530

    Minor change in files created as test cases

--- create_test_firectory.py
+++ create_test_firectory.py
@@ -7,7 +7,7 @@ def main():
     if os.path.isdir(test_folder) is False:
         os.mkdir(test_folder)
 
-    files_to_create = ['123abc.abc', 'blaabc.blamp3', 'abc.abc']
+    files_to_create = ['123xyz.txt', 'blaabc.blamp3', 'abc.abc']
 
     for file in files_to_create:
         os.system('touch ' + os.path.join(test_folder, file))
