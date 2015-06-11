d35b503be018f723341369ab6b9fa36be8e7527e
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-11 22:50:33 +0530

    Create new directories if needed

--- create_test_directory.py
+++ create_test_directory.py
@@ -2,19 +2,26 @@
 
 import os
 
+
 def main():
     test_folder = 'tmp'
-    if os.path.isdir(test_folder) is False:
-        os.mkdir(test_folder)
-
     files_to_create = ['123xyz.txt', 'blaabc.blamp3', 'abc.abc']
 
     for file in files_to_create:
-        touch(os.path.join(test_folder, file))
+        create(os.path.join(test_folder, file))
+
+
+def create(path):
+    basedir = os.path.dirname(path)
+    if not os.path.exists(basedir):
+        os.makedirs(basedir)
+    touch(path)
+
 
 def touch(fname, times=None):
     with open(fname, 'a'):
         os.utime(fname, times)
+
 
 if __name__ == '__main__':
     main()
