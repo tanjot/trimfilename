cb822b0db2c3e23b5a9883c91fd816a0a41ceff6
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-11 23:13:24 +0530

    Print file tree structure

--- create_test_directory.py
+++ create_test_directory.py
@@ -7,6 +7,7 @@ def main():
     test_folder = 'tmp'
     for file_name in get_file_structure():
         create(os.path.join(test_folder, file_name))
+    list_files(test_folder)
 
 
 def get_file_structure():
@@ -19,6 +20,18 @@ def get_file_structure():
     files.extend(level1_files)
     files.extend(level2_files)
     return files
+
+
+
+def list_files(startpath):
+    INDENT = SUBINDENT = 2
+    for root, _, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * INDENT * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * SUBINDENT * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
 
 
 def create(path):
