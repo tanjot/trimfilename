0c2466bf0066b7208a092edfd5d55c835d9f50e6
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-12 18:57:58 +0530

    Add tests for -f and --folder and no arguments

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 
+import os
 import sys
 
 try:
@@ -26,11 +27,26 @@ class TestMain(unittest.TestCase):
         sys.argv = self.argv_backup
 
     @patch('trimfilename.main.TrimFilename.parseDir')
-    def test_should_compile_folder_if_available(self, mock_parse_dir):
+    def test_should_parse_folder_if_available_with_short_flag(self, mock_parse_dir):
         mock_parse_dir.return_value = 0
         sys.argv = ['dummy', '-f', self.folder_name]
         main()
         mock_parse_dir.assert_called_once_with(self.folder_name)
+
+    @patch('trimfilename.main.TrimFilename.parseDir')
+    def test_should_parse_folder_if_available_with_long_flag(self, mock_parse_dir):
+        mock_parse_dir.return_value = 0
+        sys.argv = ['dummy', '--folder', self.folder_name]
+        main()
+        mock_parse_dir.assert_called_once_with(self.folder_name)
+
+    @patch('trimfilename.main.TrimFilename.parseDir')
+    def test_should_parse_cwd_if_no_arguments_passed(self, mock_parse_dir):
+        mock_parse_dir.return_value = 0
+        sys.argv = ['dummy']
+        main()
+        mock_parse_dir.assert_called_once_with(os.getcwd())
+
 
 class AnyStringContaining(str):
     def __eq__(self, other):
