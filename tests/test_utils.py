5b9964536b4c888a848f57b23966db1ec833bf10
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-14 05:04:59 +0530

    Mock os.rename call in test
    There need to be more test to cover different paths, this is only the happy
    path

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,21 +3,28 @@
 import unittest
 import os
 
-from trimfilename.utils import rename_file
+try:
+    import unittest
+    import unittest.mock
+    from unittest.mock import patch
+    from unittest.mock import call
+except ImportError as e:
+    import mock
+    from mock import patch
+    from mock import call
 
+from trimfilename.utils import rename_file
 
 class TestRenameFile(unittest.TestCase):
 
-    def test_rename_file_returns_true_if_filename_modified(self):
-
+    @patch('os.rename')
+    def test_rename_file_returns_true_if_filename_modified(self, mock_rename):
+        mock_rename.return_value = 0
         oldname='blaa.mp3'
         newname='laa.mp3'
         path='./'
-        open(path+oldname, 'w')
 
         output = rename_file(oldname, newname, path)
 
         self.assertTrue(output)
-
-        os.remove(path+newname)
 
