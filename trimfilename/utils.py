087582f884fd36455ffb32502afe2270a2447101
Tanjot Kaur <tanjot28@gmail.com>
2015-06-14 13:08:36 +0530

    Reorder conditions in utils: rename_file

--- trimfilename/utils.py
+++ trimfilename/utils.py
@@ -13,14 +13,14 @@ def rename_file(oldname, newname, dirPath):
 
     changed_file_name = False
 
-    if( os.path.exists((os.path.join(dirPath, newname))) is False):
-        if ( (newname.find('.') != -1) and
-             (len(oldname)-oldname.rfind('.')) == (len(newname)-newname.rfind('.')) ):
-            old_file_path = os.path.join(dirPath, oldname)
+    if oldname != newname:
+        old_file_path = os.path.join(dirPath, oldname)
+        if os.path.exists((os.path.join(dirPath, newname))) is False:
+            if (newname and (newname.find('.') != -1) and newname[0] != '.' and
+                 (len(oldname)-oldname.rfind('.')) == (len(newname)-newname.rfind('.'))):
 
-            # Does not rename file if it begins with '.' or the whole file name gets
-            # deleted after rename and also if there is no change in filename
-            if newname and newname[0] != '.' and oldname != newname:
+                # Does not rename file if it begins with '.' or the whole file name gets
+                # deleted after rename and also if there is no change in filename
                 os.rename(old_file_path, os.path.join(dirPath, newname))
                 # print('Successfully renamed '+pathNname+' to'
                 #       ' '+ newname)
