e8f7ecb5c0291cba48c1653ac14aa87a7e30ab08
Tanjot Kaur <tanjot28@gmail.com>
2015-06-18 08:15:15 +0530

    Add test, class TestAddToRenamedList: test_add_to_renamed_list_when_list_is_empty

--- tests/test_trimfilename.py
+++ tests/test_trimfilename.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import os
+
 from trimfilename.trimfilename import TrimFilename
 
 try:
@@ -23,4 +25,21 @@ class TestGetStringFormat(unittest.TestCase):
         expected_format = '{0:<' + str(len(max(list, key=len))) + '} {1}'
 
         self.assertEquals(expected_format, self.tfn_handle.get_string_format(list))
+
+
+class TestAddToRenamedList(unittest.TestCase):
+
+    def setUp(self):
+        self.tfn_handle = TrimFilename(None, None, None)
+        # self.tfn_handle.renamed_list = [ '123abc.txt':'abc.txt', '0913903bla.txt':'bla.txt']
+
+    def test_add_to_renamed_list_when_list_is_empty(self):
+        oldname = '233xyz.txt'
+        newname = 'xyz.txt'
+        path='.'
+
+        renamed_list = {os.path.join(path, oldname):newname}
+        self.tfn_handle.add_to_renamed_list(oldname, newname, path)
+
+        self.assertEqual(renamed_list,self.tfn_handle.renamed_files_list)
 
